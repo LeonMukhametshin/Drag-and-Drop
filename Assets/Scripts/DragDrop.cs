8929627b455c2c16a6c6479497ceaec1b503169c
LeonMukhametshin <leonmukhametshin2004@gmail.com>
2025-01-11 18:20:02 +0300

    fix: предотвращён выход объекта за границы экрана при перетаскивании
    - Исправлена ошибка, при которой объект мог выходить за пределы экрана
    - Добавлена проверка границ экрана и ограничение позиции перетаскиваемого объекта.

--- Assets/Scripts/DragDrop.cs
+++ Assets/Scripts/DragDrop.cs
@@ -30,6 +30,8 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, I
     public void OnDrag(PointerEventData eventData)
     {
         rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+
+        ClampToScreenBounds();
     }
 
     public void OnEndDrag(PointerEventData eventData)
@@ -42,4 +44,24 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, I
     {
         Debug.Log("OnPointerDown");
     }
+
+    private void ClampToScreenBounds()
+    {
+        Vector3 worldPosition = rectTransform.position;
+
+        RectTransform canvasRect = canvas.GetComponent<RectTransform>();
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvasRect, worldPosition, canvas.worldCamera, out Vector2 localPoint);
+
+        Vector3[] canvasCorners = new Vector3[4];
+        canvasRect.GetWorldCorners(canvasCorners);
+        Vector3 minBounds = canvasCorners[0];
+        Vector3 maxBounds = canvasCorners[2];
+
+        Vector3 clampedPosition = rectTransform.position;
+        clampedPosition.x = Mathf.Clamp(clampedPosition.x, minBounds.x, maxBounds.x);
+        clampedPosition.y = Mathf.Clamp(clampedPosition.y, minBounds.y, maxBounds.y);
+
+        rectTransform.position = clampedPosition;
+    }
+
 }
